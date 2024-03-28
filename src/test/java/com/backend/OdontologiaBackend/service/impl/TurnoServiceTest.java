470e4177d45aa1e4fe44041e9783c263e653c477
Nahuel Santos <santosnahuel98@gmail.com>
2024-03-28 18:27:01 -0300

    Subiendo el proyecto COMPLETO

--- src/test/java/com/backend/OdontologiaBackend/service/impl/TurnoServiceTest.java
+++ src/test/java/com/backend/OdontologiaBackend/service/impl/TurnoServiceTest.java
@@ -1,10 +1,13 @@
 package com.backend.OdontologiaBackend.service.impl;
 
-import com.backend.OdontologiaBackend.dto.entrada.DomicilioEntradaDto;
+
 import com.backend.OdontologiaBackend.dto.entrada.OdontologoEntradaDto;
 import com.backend.OdontologiaBackend.dto.entrada.PacienteEntradaDto;
 import com.backend.OdontologiaBackend.dto.entrada.TurnoEntradaDto;
 import com.backend.OdontologiaBackend.dto.salida.OdontologoSalidaDto;
+import com.backend.OdontologiaBackend.dto.salida.PacienteSalidaDto;
+import com.backend.OdontologiaBackend.dto.salida.TurnoSalidaDto;
+import com.backend.OdontologiaBackend.exceptions.BadRequestException;
 import com.backend.OdontologiaBackend.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -14,7 +17,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,12 +31,58 @@ class TurnoServiceTest {
     @Autowired
     private TurnoService turnoService;
 
-@Test
+    @Autowired
+    private PacienteService pacienteService;
+
+    @Autowired
+    private OdontologoService odontologoService;
+
+    @Test
     @Order(1)
-    void tatatatt(){
+    void deberiaRegistrarUnTurnoYRetornarElId() {
 
-}
+        TurnoSalidaDto turnoSalidaDto = null;
+        PacienteEntradaDto pacienteRegistrar = new PacienteEntradaDto();
+        OdontologoEntradaDto odontologoRegistrar = new OdontologoEntradaDto();
+        TurnoEntradaDto turnoEntradaDto = new TurnoEntradaDto();
 
+        pacienteRegistrar.setNombre("Miguel");
+        odontologoRegistrar.setNombre("Rodolfo");
+
+        PacienteSalidaDto pacienteSalidaDto= pacienteService.registrarPaciente(pacienteRegistrar);
+        OdontologoSalidaDto odontologoSalidaDto = odontologoService.registrar(odontologoRegistrar);
+
+        turnoEntradaDto.setPacienteId(pacienteSalidaDto.getId());
+        turnoEntradaDto.setOdontologoId(odontologoSalidaDto.getId());
+
+        try {
+            turnoSalidaDto = turnoService.registrar(turnoEntradaDto);
+        } catch (BadRequestException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertNotNull(turnoSalidaDto.getId());
+    }
+
+    @Test
+    @Order(2)
+    void deberiaBorrarElTurnoConElId1() {
+        TurnoEntradaDto turnoEntradaDto = new TurnoEntradaDto(1L,1L, LocalDateTime.of (2025,2,12, 9,30 ));
+
+        try{
+            turnoService.eliminarTurno(1L);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        assertNull(turnoService.buscarTurno(1L));
+    }
+
+    @Test
+    @Order(3)
+    void deberiaDevolverUnaListaVacia(){
+        List<TurnoSalidaDto> turnoSalidaDtos = turnoService.listarTurnos();
+        assertTrue(turnoSalidaDtos.isEmpty());
+    }
 
 
 }
